1de99b7cd202d75676a581512756fc64bc835244
Javier López <javierlopez@uc.cl>
2019-09-01 12:37:49 -0400

    current user mw

--- api/index.js
+++ api/index.js
@@ -8,23 +8,24 @@ config.config();
 
 const app = express();
 
+// parse request
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// session
 app.use(cookieSession({
   keys: JSON.parse(process.env.SECRET_KEYS),
   maxAge: 14 * 24 * 60 * 60 * 1000, // 2 weeks
 }));
 
+// routing
 app.use('/api/v1', routes);
-
-// when a random route is inputed
 app.get('*', (req, res) => res.status(200).send({
   message: 'Welcome to this API.',
 }));
 
+// app
 const port = process.env.PORT || 8000;
-
 app.listen(port, () => {
   /* eslint-disable-next-line no-console */
   console.log(`Server is running on PORT ${port}`);
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -3,8 +3,14 @@ import AuthorRoutes from './AuthorRoutes';
 import BookRoutes from './BookRoutes';
 import SessionRoutes from './SessionRoutes';
 import UserRoutes from './UserRoutes';
+import { User } from '../models';
 
 const router = Router();
+
+router.use(async (req, res, next) => {
+  req.currentUser = req.session.userId && await User.findByPk(req.session.userId);
+  return next();
+});
 
 router.use('/authors', AuthorRoutes);
 router.use('/books', BookRoutes);
